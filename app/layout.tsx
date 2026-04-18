589273245dc2deebb16afad7c5fff31ff1a0cd24
positiveppl <ajstout@gmail.com>
2026-04-18 16:16:39 -0700

    added export cont to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,11 @@ export const metadata: Metadata = {
   viewport: 'width=device-width, initial-scale=1.0',
 }
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
