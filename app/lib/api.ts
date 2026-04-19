77b81514a558a2c01a119b3011fc1ce54981f372
positiveppl <ajstout@gmail.com>
2026-04-18 18:15:23 -0700

    fixed api.ts export const

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE = 'https://api-gateway.msgpnn.workers.dev'
+export const API_BASE = 'https://api-gateway.fixsactransit.workers.dev'
 
 export interface CityScore {
   id: string
