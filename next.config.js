fd80d3c7f70f71725685800d8d2757e789ce585a
positiveppl <ajstout@gmail.com>
2026-04-20 06:22:06 -0700

    restore static export - site back up

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   env: {
-    NEXT_PUBLIC_MAPBOX_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_TOKEN || 'pk.eyJ1IjoicG9zaXRpdmVwcGwiLCJhIjoiY21tbXdocTBxMmQ0czJwb2Y3ejBmYmp2MiJ9.3HS4ay8Kq6OIJlv06lZr3Q',
+    NEXT_PUBLIC_MAPBOX_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_TOKEN || 'pk.eyJ1...',
   },
 }
-
-module.exports = nextConfig
